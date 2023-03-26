d3386931bec927895a506f6ea1772422e4623032
wsm8855 <wsm8855@g.rit.edu>
2023-03-26 01:56:36 -0400

    Add tests for recommendation and remove old placeholders

--- tests.py
+++ tests.py
@@ -7,13 +7,13 @@ print("Sending request...")
 response = requests.get(SERVER)
 print(response)
 
-print("Sending text request (good)...")
-response = requests.post(SERVER + "api/text/", data=json.dumps({"text": "echo!"}))
-print(response.json())
-
-print("Sending text request (bad)...")
-response = requests.post(SERVER + "api/text/", data=json.dumps({"wrong_field_name": "this should fail"}))
-print(response.json())
+# print("Sending text request (good)...")
+# response = requests.post(SERVER + "api/text/", data=json.dumps({"text": "echo!"}))
+# print(response.json())
+#
+# print("Sending text request (bad)...")
+# response = requests.post(SERVER + "api/text/", data=json.dumps({"wrong_field_name": "this should fail"}))
+# print(response.json())
 
 print("Sending categorical query (good)")
 response = requests.post(SERVER + "api/categoricalQuery/",
@@ -22,5 +22,21 @@ response = requests.post(SERVER + "api/categoricalQuery/",
                              "ethnicities": ["Caucasian"],
                              "genders": ["Female"],
                              "states": ["New york"],
+                         }))
+print(response.json())
+
+print("Sending recommendation query (good)")
+response = requests.post(SERVER + "api/text/",
+                         data=json.dumps({
+                             "text": None,
+                             "questionUno": "9AA6F105-BE5F-4A9A-876A-B118236151FC"
+                         }))
+print(response.json())
+
+print("Sending recommendation query (bad)")
+response = requests.post(SERVER + "api/text/",
+                         data=json.dumps({
+                             "text": None,
+                             "questionUno": None
                          }))
 print(response.json())
